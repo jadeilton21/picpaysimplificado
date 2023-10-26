7ce99c2d30421e2e0d58530ca3e5028b89f911da
jadeilton <jadeiltondm@gmail.com>
2023-10-25 22:04:07 -0300

    Feat : TransactionRepository create

--- src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/transaction/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/transaction/TransactionRepository.java
@@ -1,4 +1,6 @@
 package com.picpaysimplificado.picpaysimplificado.doMain.transaction;
 
-public interface TransactionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 }
