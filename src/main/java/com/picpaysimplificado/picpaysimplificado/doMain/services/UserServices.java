86e3dadac1ac2eb28aae539c46d3dd8a3b5229ec
jadeilton <jadeiltondm@gmail.com>
2023-10-26 14:21:30 -0300

    Feat : Create UserServices

--- src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/services/UserServices.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/services/UserServices.java
@@ -1,9 +1,12 @@
 package com.picpaysimplificado.picpaysimplificado.doMain.services;
 
 
+import com.picpaysimplificado.picpaysimplificado.doMain.user.User;
 import com.picpaysimplificado.picpaysimplificado.doMain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class UserServices {
@@ -13,6 +16,12 @@ public class UserServices {
 
     @Autowired
     private UserRepository userRepository;
+
+
+
+    public void validateTransection(User sender, BigDecimal amout) throws Exception{
+
+    }
 
 
 }
