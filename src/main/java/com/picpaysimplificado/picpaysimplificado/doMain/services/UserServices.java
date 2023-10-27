3883c04300a7d972aa0546e752a8c746619869b7
jadeilton <jadeiltondm@gmail.com>
2023-10-27 10:33:19 -0300

    Feat : Configurando Services.

--- src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/services/UserServices.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/services/UserServices.java
@@ -1,12 +1,14 @@
 package com.picpaysimplificado.picpaysimplificado.doMain.services;
 
 
+import com.picpaysimplificado.picpaysimplificado.doMain.user.TypeUser;
 import com.picpaysimplificado.picpaysimplificado.doMain.user.User;
 import com.picpaysimplificado.picpaysimplificado.doMain.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+
 
 @Service
 public class UserServices {
@@ -21,7 +23,26 @@ public class UserServices {
 
     public void validateTransection(User sender, BigDecimal amout) throws Exception{
 
+        if(sender.getTypeUser() == TypeUser.LOGISTAS){
+            throw new Exception("O úsuario do Tipo Lógista não está autorizado a fazer a transação.");
+        }
+
+        if(sender.getBalance().compareTo(amout) < 0){
+            throw new Exception("O seu saldo é Insuficiente");
+        }
+
     }
 
+
+    public User findUserById(Long id) throws Exception{
+
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usúario não Encontrado."));
+
+    }
+
+
+    public void saverUser(User user){
+        this.userRepository.save(user);
+    }
 
 }
