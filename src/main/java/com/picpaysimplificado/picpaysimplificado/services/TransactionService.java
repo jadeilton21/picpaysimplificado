83b1388bf426a77457f5cbf15e3d05835b9b4d51
jadeilton <jadeiltondm@gmail.com>
2023-10-31 15:31:29 -0300

    Feat : update class TransactionService

--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/TransactionService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 
 
 @Service
@@ -33,6 +34,17 @@ public class TransactionService {
 
         userServices.validateTransection(sender,transaction.value());
 
+
+        boolean isAuthorized = this.authorizedTrasaction(sender,transaction.value()){
+            if(!this.authorizedTrasaction(sender, transaction.value())){
+                throw new Exception("Transação não Autorizada.")
+            }
+        }
+
+
+    }
+
+    public boolean authorizedTrasaction(User sender, BigDecimal value){
 
     }
 }
