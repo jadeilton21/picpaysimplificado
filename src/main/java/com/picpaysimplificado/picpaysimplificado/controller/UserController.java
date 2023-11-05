26709a96ed1495614757afb840fe2f79302641c1
jadeilton <jadeiltondm@gmail.com>
2023-11-05 19:15:56 -0300

    feat : Create method lisUser.

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
@@ -6,9 +6,12 @@ import com.picpaysimplificado.picpaysimplificado.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController("/users")
 public class UserController {
@@ -20,9 +23,17 @@ public class UserController {
 
 
     @PostMapping
-    private ResponseEntity<User> createUser(UserDTO userDTO){
+    public ResponseEntity<User> createUser(UserDTO userDTO){
         User newUser = userServices.createUser(userDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
 
     }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUser(){
+
+
+    }
+
+
 }
