bec835e9fee048320bd9d36dffd3c5f3819a1667
jadeilton <jadeiltondm@gmail.com>
2023-11-05 18:51:21 -0300

    feat : updaat class UserController

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.picpaysimplificado.picpaysimplificado.controller;
 
+import com.picpaysimplificado.picpaysimplificado.doMain.user.User;
+import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
+import com.picpaysimplificado.picpaysimplificado.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/users")
 public class UserController {
+
+
+
+    @Autowired
+    private UserServices userServices;
+
+
+    @PostMapping
+    private ResponseEntity<User> createUser(UserDTO userDTO){
+
+
+
+
+    }
 }
