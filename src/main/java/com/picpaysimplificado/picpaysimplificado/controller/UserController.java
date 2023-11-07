a9463bdba3211bcbb27cf46dd6f0035db7e8d507
jadeilton <jadeiltondm@gmail.com>
2023-11-06 22:53:23 -0300

    feat : update UserController in ResquestMapping

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/users")
+@RestController()
+@RequestMapping("/users")
 public class UserController {
 
 
