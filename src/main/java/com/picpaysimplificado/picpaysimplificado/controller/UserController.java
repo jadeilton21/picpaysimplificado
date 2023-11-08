57dd748bd6df3f6560cf5b5ce18646bb7347e113
jadeilton <jadeiltondm@gmail.com>
2023-11-08 16:04:03 -0300

    feat : create method excluir, para exlcuir users

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.picpaysimplificado.picpaysimplificado.controller;
 
 import com.picpaysimplificado.picpaysimplificado.doMain.user.User;
+import com.picpaysimplificado.picpaysimplificado.doMain.user.UserRepository;
 import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.picpaysimplificado.services.UserServices;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +21,14 @@ public class UserController {
 
     @Autowired
     private UserServices userServices;
+    @Autowired
+    private UserRepository repository;
 
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody @Valid UserDTO userDTO){
         User newUser = userServices.createUser(userDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
-
     }
 
     @GetMapping
@@ -33,6 +36,15 @@ public class UserController {
 
         List<User> users = this.userServices.getAllUsers();
         return new ResponseEntity<>(users,HttpStatus.OK);
+    }
+    @Transactional
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluir(@PathVariable Long id){
+        var user = repository.getReferenceById(id);
+
+        user.excluir();
+
+        return ResponseEntity.noContent().build();
 
     }
 
