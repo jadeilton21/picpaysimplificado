43f4f0d81ef9927c1a7a54b41d8ca0bedb537340
jadeilton <jadeiltondm@gmail.com>
2023-11-09 17:57:06 -0300

    feat : Create method createTransaction na classe TransacionalController

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controller/TransacionalController.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/TransacionalController.java
@@ -1,6 +1,15 @@
 package com.picpaysimplificado.picpaysimplificado.controller;
 
 
+import com.picpaysimplificado.picpaysimplificado.doMain.transaction.Transaction;
+import com.picpaysimplificado.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.picpaysimplificado.services.TransactionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/transactions")
 public class TransacionalController {
 
+        @Autowired
+        private TransactionService transactionService;
 
 
+        @PostMapping
+        public ResponseEntity<Transaction> createTransaction(@RequestBody @Valid TransactionDTO trasaction) throws Exception{
+            Transaction newTransaction = transactionService.createTransaction(trasaction);
 
+
+        }
 }
