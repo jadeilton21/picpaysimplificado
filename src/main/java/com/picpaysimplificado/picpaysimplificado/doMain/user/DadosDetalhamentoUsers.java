553bd58370655ced7172ebaf639b8a8b5b7735f3
jadeilton <jadeiltondm@gmail.com>
2023-11-09 17:07:38 -0300

    feat : create record DadosDetalhamentoUsers, para detalhar users.

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/user/DadosDetalhamentoUsers.java
@@ -0,0 +1,11 @@
+package com.picpaysimplificado.picpaysimplificado.doMain.user;
+
+import java.math.BigDecimal;
+
+public record DadosDetalhamentoUsers(Long id, String nome, String sobreNome, String document, String email, String passoword, BigDecimal balance, TypeUser typeUser) {
+
+
+    public DadosDetalhamentoUsers (User user){
+        this(user.getId(),user.getNome(),user.getSobreNome(),user.getDocument(),user.getEmail(),user.getPassoword(),user.getBalance(),user.getTypeUser());
+    }
+}
