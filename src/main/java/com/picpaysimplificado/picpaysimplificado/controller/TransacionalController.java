fb03ea16d5628827d6fc3931509c315963f23390
jadeilton <jadeiltondm@gmail.com>
2023-11-09 17:21:46 -0300

    feat : Update Class TransacionalController

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controller/TransacionalController.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/TransacionalController.java
@@ -1,4 +1,14 @@
 package com.picpaysimplificado.picpaysimplificado.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransacionalController {
+
+
+
+
 }
