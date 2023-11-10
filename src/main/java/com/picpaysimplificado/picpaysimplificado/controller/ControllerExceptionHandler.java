76d2f16c74c814fc6c2c0670bdf37c61aeb30479
jadeilton <jadeiltondm@gmail.com>
2023-11-10 10:49:34 -0300

    'feat : create class ControllerExceptionHandler. Tratar Exceptions dos controlles.

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.picpaysimplificado.picpaysimplificado.controller;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+}
