93ff3eff86171196425d885990ed1073fafc8873
jadeilton <jadeiltondm@gmail.com>
2023-11-13 07:49:22 -0300

    feat : Create Tests Unitarios, Repository User.

---
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/doMain/user/UserRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.picpaysimplificado.picpaysimplificado.doMain.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+
+
+    @Test
+    void findUserByDocument(){
+
+    }
+}
