905aa4b650cadb0027ee9ca1c544c27bc8a384eb
jadeilton <jadeiltondm@gmail.com>
2023-11-12 21:23:02 -0300

    feat : create method threatDuplicationEntry para tratar Exceptions statusCode: 400.

--- src/main/java/com/picpaysimplificado/picpaysimplificado/controller/ControllerExceptionHandler.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/controller/ControllerExceptionHandler.java
@@ -1,8 +1,31 @@
 package com.picpaysimplificado.picpaysimplificado.controller;
 
 
+import com.picpaysimplificado.picpaysimplificado.dtos.ExceptionDTO;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+
+
+        @ExceptionHandler(DataIntegrityViolationException.class)
+        public ResponseEntity threatDuplicationEntry(DataIntegrityViolationException exception){
+
+            ExceptionDTO exceptionDTO = new ExceptionDTO("Usúario já exite.","400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
