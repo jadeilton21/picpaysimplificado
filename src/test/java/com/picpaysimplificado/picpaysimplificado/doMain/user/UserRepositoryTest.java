20e9db1a54663201316d46815d77260866d7e97d
jadeilton <jadeiltondm@gmail.com>
2023-11-14 09:43:26 -0300

    feat : Update Class UserRepositoryTest, testar o UserRepository

--- src/test/java/com/picpaysimplificado/picpaysimplificado/doMain/user/UserRepositoryTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/doMain/user/UserRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.picpaysimplificado.picpaysimplificado.doMain.user;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,9 +15,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserRepositoryTest {
 
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
+    @DisplayName("Should get User successFully from 08")
     void findUserByDocument(){
+
+        String document = "999999999901";
+
 
     }
 }
