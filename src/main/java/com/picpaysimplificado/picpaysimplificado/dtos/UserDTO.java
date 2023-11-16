d309339f0bbe0ec7158f64a6b8fd9d20d06a1411
jadeilton <jadeiltondm@gmail.com>
2023-11-16 10:46:11 -0300

    feat : update UserDTO

--- src/main/java/com/picpaysimplificado/picpaysimplificado/dtos/UserDTO.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/dtos/UserDTO.java
@@ -4,6 +4,5 @@ import com.picpaysimplificado.picpaysimplificado.doMain.user.TypeUser;
 
 import java.math.BigDecimal;
 
-public record UserDTO(String nome, String sobreNome, String email, String passoword, BigDecimal balance, String document,
-                      TypeUser typeUser) {
+public record UserDTO(String nome, String sobreNome, String email, String passoword, BigDecimal balance, String document, TypeUser typeUser) {
 }
