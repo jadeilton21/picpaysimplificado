667a3702877c676633ed04e5a19edf802ccf6639
jadeilton <jadeiltondm@gmail.com>
2023-11-17 07:35:44 -0300

    feat : create test UserTest

---
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/doMain/user/UserTest.java
@@ -0,0 +1,41 @@
+package com.picpaysimplificado.picpaysimplificado.doMain.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class UserTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    @DisplayName("Deve Devolver Código http 400 quando informações forem invalidas..")
+    @WithMockUser()
+    void excluir()throws Exception {
+
+        var response = mockMvc
+                .perform(post("/users"))
+                .andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.FORBIDDEN.value());
+
+    }
+}
