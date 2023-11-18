ff7c8a8c1cd85d09d1d7682c76e1da1686f0fcf2
jadeilton <jadeiltondm@gmail.com>
2023-11-18 17:39:14 -0300

    feat : update record DadosDetalhamentoUsers

--- src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/user/DadosDetalhamentoUsers.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/doMain/user/DadosDetalhamentoUsers.java
@@ -9,14 +9,4 @@ public record DadosDetalhamentoUsers(Long id, String nome, String sobreNome, Str
         this(user.getId(),user.getNome(),user.getSobreNome(),user.getDocument(),user.getEmail(),user.getPassoword(),user.getBalance(),user.getTypeUser());
     }
 
-    public DadosDetalhamentoUsers(Long id, String nome, String sobreNome, String document, String email, String passoword, BigDecimal balance, TypeUser typeUser) {
-        this.id = id;
-        this.nome = nome;
-        this.sobreNome = sobreNome;
-        this.document = document;
-        this.email = email;
-        this.passoword = passoword;
-        this.balance = balance;
-        this.typeUser = typeUser;
-    }
 }
