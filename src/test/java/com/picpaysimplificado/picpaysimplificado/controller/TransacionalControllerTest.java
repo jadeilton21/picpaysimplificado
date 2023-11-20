3222c646fc944a9e102625bfb0e566f1d5149d28
jadeilton <jadeiltondm@gmail.com>
2023-11-20 20:25:02 -0300

    feat : method createTransaction_cenario1 tested.

--- src/test/java/com/picpaysimplificado/picpaysimplificado/controller/TransacionalControllerTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/controller/TransacionalControllerTest.java
@@ -1,12 +1,41 @@
 package com.picpaysimplificado.picpaysimplificado.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+@SpringBootTest
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc
 class TransacionalControllerTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void createTransaction() {
+    @DisplayName("Deve Devolver Código Http 403 quando informações estiverem invalidas..")
+    @WithMockUser
+    void createTransaction_cenario1() throws Exception{
+
+        var response = mockMvc
+                .perform(post("/users"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.FORBIDDEN.value());
+
+
+
     }
 }
