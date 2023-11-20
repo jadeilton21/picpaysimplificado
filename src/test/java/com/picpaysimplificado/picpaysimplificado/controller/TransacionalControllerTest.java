530440e2020daa9566d2693250a4a5e8bf9d2156
jadeilton <jadeiltondm@gmail.com>
2023-11-20 20:16:48 -0300

    feat : create class test TransacionalControllerTest

---
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/controller/TransacionalControllerTest.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.picpaysimplificado.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransacionalControllerTest {
+
+    @Test
+    void createTransaction() {
+    }
+}
