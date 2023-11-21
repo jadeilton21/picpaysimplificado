d0ec4c64b3cf54fe821dcdb9b681223e0afa39f7
jadeilton <jadeiltondm@gmail.com>
2023-11-21 18:31:29 -0300

    feat: testing method sendNotification from the class NotificationService.

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/NotificationServiceTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/NotificationServiceTest.java
@@ -1,9 +1,11 @@
 package com.picpaysimplificado.picpaysimplificado.services;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -11,6 +13,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureMockMvc
 class NotificationServiceTest {
 
+    @Autowired
+    private MockMvc mockMvc;
     @Test
     void sendNotification() {
     }
