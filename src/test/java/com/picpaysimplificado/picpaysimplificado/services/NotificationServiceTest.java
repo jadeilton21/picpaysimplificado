24728cbc26f24b0c5cec7ada09ed1e6b6a9294ff
jadeilton <jadeiltondm@gmail.com>
2023-11-21 18:29:18 -0300

    feat: creating test the class service NotificationService.

---
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/NotificationServiceTest.java
@@ -0,0 +1,17 @@
+package com.picpaysimplificado.picpaysimplificado.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc
+class NotificationServiceTest {
+
+    @Test
+    void sendNotification() {
+    }
+}
