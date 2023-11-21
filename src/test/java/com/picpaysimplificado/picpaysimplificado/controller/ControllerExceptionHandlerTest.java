9bfc9307184ca127ca3e5b29b27029fcc2c3849a
jadeilton <jadeiltondm@gmail.com>
2023-11-21 17:41:47 -0300

    feat : creating argument inseide the methodo theatDuplicatinoEntry inseide the test class ControllerExceptionHandlerTest.

--- src/test/java/com/picpaysimplificado/picpaysimplificado/controller/ControllerExceptionHandlerTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/controller/ControllerExceptionHandlerTest.java
@@ -1,12 +1,32 @@
 package com.picpaysimplificado.picpaysimplificado.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+@SpringBootTest
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc
 class ControllerExceptionHandlerTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void threatDuplicationEntry() {
+    @DisplayName("Deve Devolver Código Http 403 quando informações estiverem invalidas...")
+    @WithMockUser
+    void threatDuplicationEntry() throws Exception{
+
     }
 }
