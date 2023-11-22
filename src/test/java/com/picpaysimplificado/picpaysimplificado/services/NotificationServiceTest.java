0b1649c513292dfcd4767d92f6b1b6c85657fdc5
jadeilton <jadeiltondm@gmail.com>
2023-11-22 10:47:31 -0300

    feat: tested method sendNotification_cenario1 in class NotificationServiceTest

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/NotificationServiceTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/NotificationServiceTest.java
@@ -1,11 +1,13 @@
 package com.picpaysimplificado.picpaysimplificado.services;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +22,8 @@ class NotificationServiceTest {
     @Autowired
     private MockMvc mockMvc;
     @Test
+    @DisplayName("Deve Devolver Código Http 403 quando Informações estiverem Invalidas..")
+    @WithMockUser
     void sendNotification_cenario1() throws Exception{
 
         var response = mockMvc
