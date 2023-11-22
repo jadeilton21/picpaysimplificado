afb2fe296781e54416fd6f1d09cab152bb530a42
jadeilton <jadeiltondm@gmail.com>
2023-11-22 07:30:18 -0300

    feat: update class NotificationServiceTest

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/NotificationServiceTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/NotificationServiceTest.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @SpringBootTest
 @AutoConfigureJsonTesters
 @AutoConfigureMockMvc
@@ -16,6 +20,9 @@ class NotificationServiceTest {
     @Autowired
     private MockMvc mockMvc;
     @Test
-    void sendNotification() {
+    void sendNotification_cenario1() throws Exception{
+
+
+
     }
 }
