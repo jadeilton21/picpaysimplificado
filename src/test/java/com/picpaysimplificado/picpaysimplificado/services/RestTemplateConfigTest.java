8d054a35827cc08ee892fd7ba1d1e3b03d5837ea
jadeilton <jadeiltondm@gmail.com>
2023-11-22 16:54:01 -0300

    feat: Method restTemplate tested in class RestTemplateConfigTest

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/RestTemplateConfigTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/RestTemplateConfigTest.java
@@ -2,21 +2,37 @@ package com.picpaysimplificado.picpaysimplificado.services;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureJsonTesters
 @AutoConfigureMockMvc
 class RestTemplateConfigTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     @DisplayName("Deve Devolver Código Http 403 quando Informações estiverem Invalidas..")
     @WithMockUser
     void restTemplate() throws Exception {
+
+        var response = mockMvc
+                .perform(post("/users"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.FORBIDDEN.value());
     }
 }
