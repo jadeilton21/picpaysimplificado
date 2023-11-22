d74113934ab884c6a97710e0ae5249257dc553e0
jadeilton <jadeiltondm@gmail.com>
2023-11-22 13:38:40 -0300

    feat: creating test restTemplate for class RestTemplateConfigTest

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/RestTemplateConfigTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/RestTemplateConfigTest.java
@@ -1,12 +1,22 @@
 package com.picpaysimplificado.picpaysimplificado.services;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc
 class RestTemplateConfigTest {
 
     @Test
-    void restTemplate() {
+    @DisplayName("Deve Devolver Código Http 403 quando Informações estiverem Invalidas..")
+    @WithMockUser
+    void restTemplate() throws Exception {
     }
 }
