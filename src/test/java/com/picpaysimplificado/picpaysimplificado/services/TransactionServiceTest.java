d159868949871964aa4450d9626ae3c1fc500812
jadeilton <jadeiltondm@gmail.com>
2023-11-23 14:14:18 -0300

    feat: method createTransaction_cenario1 in class TransactionServiceTest, tested....

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/TransactionServiceTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/TransactionServiceTest.java
@@ -2,12 +2,17 @@ package com.picpaysimplificado.picpaysimplificado.services;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -15,9 +20,23 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureMockMvc
 class TransactionServiceTest {
 
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     @DisplayName("Deve Devolver Código http 403 quando informações estiverem Invalidas..")
     @WithMockUser
-    void createTransaction() {
+    void createTransaction_cenario1() throws Exception {
+
+        var response = mockMvc
+                .perform(post("/users"))
+                .andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.FORBIDDEN.value());
+
+
     }
 }
