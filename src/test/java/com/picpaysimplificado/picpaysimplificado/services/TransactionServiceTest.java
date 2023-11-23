d5eab811b8c54481f651a1801af68f80edbbaf67
jadeilton <jadeiltondm@gmail.com>
2023-11-23 09:55:18 -0300

    feat: creating class test TransactionServiceTest

---
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/TransactionServiceTest.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.picpaysimplificado.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceTest {
+
+    @Test
+    void createTransaction() {
+    }
+}
