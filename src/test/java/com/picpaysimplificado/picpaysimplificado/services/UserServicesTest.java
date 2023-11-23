49f93479b772943b7cb68f45bd09130c0f2e68f0
jadeilton <jadeiltondm@gmail.com>
2023-11-23 14:32:52 -0300

    feat: method validateTransection_cenario1 in class UserServiceTest,tested...

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/UserServicesTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/UserServicesTest.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureJsonTesters
@@ -23,7 +26,15 @@ class UserServicesTest {
     @Test
     @DisplayName("Deve Devolver Código http 403 quando informações estiverem invalidas...")
     @WithMockUser
-    void validateTransection_cenario1() {
+    void validateTransection_cenario1() throws Exception{
+
+        var response = mockMvc
+                .perform(post("/users"))
+                .andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.FORBIDDEN.value());
 
     }
 }
