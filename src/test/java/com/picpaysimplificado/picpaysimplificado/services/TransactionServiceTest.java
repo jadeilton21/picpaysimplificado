22e9c11b346b7cf23e65ff61b5d871f8e259124e
jadeilton <jadeiltondm@gmail.com>
2023-11-23 09:58:31 -0300

    feat: creating method createTransaction_cenario1 to test.

--- src/test/java/com/picpaysimplificado/picpaysimplificado/services/TransactionServiceTest.java
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/services/TransactionServiceTest.java
@@ -1,12 +1,23 @@
 package com.picpaysimplificado.picpaysimplificado.services;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@AutoConfigureJsonTesters
+@AutoConfigureMockMvc
 class TransactionServiceTest {
 
     @Test
+    @DisplayName("Deve Devolver Código http 403 quando informações estiverem Invalidas..")
+    @WithMockUser
     void createTransaction() {
     }
 }
